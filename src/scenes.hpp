dc8a232bc001ce414a1200c77b7b3f3173403216
ibuki2003 <main@fuwa.dev>
2020-10-22 17:22:56 +0900

    refactor: format codes

--- src/scenes.hpp
+++ src/scenes.hpp
@@ -1,9 +1,9 @@
 #pragma once
-#include <Siv3D.hpp> // OpenSiv3D v0.4.3
+#include <Siv3D.hpp>  // OpenSiv3D v0.4.3
 #include <common.hpp>
-#include <scenes/Title.hpp>
 #include <scenes/Description.hpp>
 #include <scenes/Game/Game.hpp>
 #include <scenes/Results.hpp>
+#include <scenes/Title.hpp>
 
 App& registerScenes(App&);
